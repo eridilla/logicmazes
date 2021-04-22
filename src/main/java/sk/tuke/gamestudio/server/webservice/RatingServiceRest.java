23cf334ac41c7faa4717d338e7c051fbd08be74c
Eduard Ridilla <eduard.ridilla@student.tuke.sk>
2021-04-22 22:29:21 +0200

    rest

---
+++ src/main/java/sk/tuke/gamestudio/server/webservice/RatingServiceRest.java
@@ -0,0 +1,24 @@
+package sk.tuke.gamestudio.server.webservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import sk.tuke.gamestudio.entity.Rating;
+import sk.tuke.gamestudio.service.RatingService;
+
+@RestController
+@RequestMapping("/api/rating")
+public class RatingServiceRest {
+
+    @Autowired
+    private RatingService ratingService;
+
+    @GetMapping("/{game}")
+    public int getComments(@PathVariable String game) {
+        return ratingService.getAverageRating(game);
+    }
+
+    @PostMapping
+    public void addComment(@RequestBody Rating rating) {
+        ratingService.setRating(rating);
+    }
+}
